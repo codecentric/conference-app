716b82e375d79e8394e6150717ac6c2ff234eaed
Nils Wloka <nils.wloka@codecentric.de>
2014-08-21 14:05:15 +0200

    Reorganized imports

--- thot-app/src/main/java/org/ale/thot/web/controller/AllSessionsController.java
+++ thot-app/src/main/java/org/ale/thot/web/controller/AllSessionsController.java
@@ -1,11 +1,5 @@
 package org.ale.thot.web.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.ale.thot.dao.LocationDao;
 import org.ale.thot.dao.SessionDao;
 import org.ale.thot.dao.TimeslotDao;
@@ -18,6 +12,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/allSessions")
