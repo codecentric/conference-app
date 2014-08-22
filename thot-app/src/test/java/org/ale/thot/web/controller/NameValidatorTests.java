0f66bc8d1ddfc5118d8440c060cb541de8e0f4fb
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-08-22 10:11:54 +0300

    Added tests for NameValidator

--- thot-app/src/test/java/org/ale/thot/web/controller/NameValidatorTests.java
+++ thot-app/src/test/java/org/ale/thot/web/controller/NameValidatorTests.java
@@ -9,6 +9,50 @@ import static org.junit.Assert.assertTrue;
  * Created by adi on 8/21/14.
  */
 public class NameValidatorTests {
+
+    @Test
+    public void nameValidWhenContainsNumbers(){
+        NameValidator nameValidator = new NameValidator();
+        String emptySpeaker = "@243";
+
+        assertTrue(nameValidator.isValid(emptySpeaker));
+    }
+
+    @Test
+    public void nameValidWhenContainsLowerCaseLetters(){
+        NameValidator nameValidator = new NameValidator();
+        String emptySpeaker = "@validname";
+
+        assertTrue(nameValidator.isValid(emptySpeaker));
+    }
+
+
+    @Test
+    public void nameValidWhenContainsUpperCaseLetters(){
+        NameValidator nameValidator = new NameValidator();
+        String emptySpeaker = "@ValidName";
+
+        assertTrue(nameValidator.isValid(emptySpeaker));
+    }
+
+
+    @Test
+    public void nameValidWhenContainsUnderscore(){
+        NameValidator nameValidator = new NameValidator();
+        String emptySpeaker = "@some_name";
+
+        assertTrue(nameValidator.isValid(emptySpeaker));
+    }
+
+
+    @Test
+    public void nameNotValidWhenContainsDash(){
+        NameValidator nameValidator = new NameValidator();
+        String emptySpeaker = "@some-name";
+
+        assertFalse(nameValidator.isValid(emptySpeaker));
+    }
+
     @Test
     public void nameIsNotValidWhenEmpty(){
         NameValidator nameValidator = new NameValidator();
@@ -21,6 +65,14 @@ public class NameValidatorTests {
     public void nameIsNotValidWhenNotStartingWithAt(){
         NameValidator nameValidator = new NameValidator();
         String emptySpeaker = "@";
+
+        assertFalse(nameValidator.isValid(emptySpeaker));
+    }
+
+    @Test
+    public void nameIsNotValidWhenContainsSpace(){
+        NameValidator nameValidator = new NameValidator();
+        String emptySpeaker = "@some name";
 
         assertFalse(nameValidator.isValid(emptySpeaker));
     }
