f6263048b98f48b92473452fc2430816cea3960f
Marcel Birkner <marcel.birkner@gmail.com>
2015-03-24 12:24:14 +0100

    fixed deploy script

--- app/scripts/deploy.sh
+++ app/scripts/deploy.sh
@@ -8,17 +8,15 @@ echo "Deploying Version $VERSION to $ENVIRONMENT"
 APP_HOME=/opt/conference-app
 REPOSITORY=http://54.93.92.96:8081/artifactory/simple/libs-release-local
 
-if[ "$ENVIRONMENT" == "JAVALAND_PROD_A" ]
-then
+if [ "$ENVIRONMENT" == "JAVALAND_PROD_A" ]; then
   SERVER="54.93.219.237"
-elif[ "$ENVIRONMENT" == "JAVALAND_STAGING" ]
-then
+elif [ "$ENVIRONMENT" == "JAVALAND_STAGING" ]; then
   SERVER="52.28.10.112"
-elif[ "$ENVIRONMENT" == "JAVALAND_TEST" ]
-then
+elif [ "$ENVIRONMENT" == "JAVALAND_TEST" ]; then
   SERVER="52.28.14.105"
 else
   echo "Stopping Deployment, please provide valide ENVIRONMENT variable"
+  exit 1
 fi
  
 echo "Stopping Java Process on $SERVER"
